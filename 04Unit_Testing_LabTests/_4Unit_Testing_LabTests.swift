1ed1a13b5d056b3af58278e489bbdd6977afa7fe
aglegaspi <lex83nyc@gmail.com>
2019-08-29 14:21:39 -0400

    first commit

---
+++ 04Unit_Testing_LabTests/_4Unit_Testing_LabTests.swift
@@ -0,0 +1,34 @@
+//
+//  _4Unit_Testing_LabTests.swift
+//  04Unit_Testing_LabTests
+//
+//  Created by Alexander George Legaspi on 8/29/19.
+//  Copyright © 2019 Alexander George Legaspi. All rights reserved.
+//
+
+import XCTest
+@testable import _4Unit_Testing_Lab
+
+class _4Unit_Testing_LabTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
